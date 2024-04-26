bf9a08927433432175930b72dd991ab435412597
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-25 22:30:24 -0300

    minha vó

--- insert_cliente.php
+++ insert_cliente.php
@@ -1,15 +1,15 @@
 <?php 
     include 'conexao.php';
 
-    $nome = $_POST["nome"];
-    $endereco = $_POST["endereco"];
-    $nrendereco = $_post ["nrendereco"];
-    $cpf = $_POST["cpf"];
-    $cnpj = $_POST["cnpj"];
-    $telefone = $_POST["telefone"];
+    $nome = $_POST['nome'];
+    $endereco = $_POST['endereco'];
+    $nrendereco = $_POST['nrendereco'];
+    $cpf = $_POST['cpf'];
+    $cnpj = $_POST['cnpj'];
+    $telefone = $_POST['telefone'];
 
-    $insert = "INSERT INTO tb_cliente VALUES (NULL,'nome','endereco','nrendereco','cpf','cnpj','telefone')";
+    $insert = "INSERT INTO tb_cliente VALUES (NULL,'$nome','$endereco',$nrendereco,'$cpf','$cnpj','$telefone')";
     
-    $query = mysqli_query($conexao, $insert) or die(mysqli_error($conexao));
+    $query = mysqli_query($conexao, $insert);
  
 ?>
--- insert_user.php
+++ insert_user.php
@@ -1,12 +1,12 @@
 <?php 
-$nome = $_POST["nome"];
-$login = $_POST["login"];
-$setor = $_POST["setor"];
-$senha = $_POST["senha"];
+$nome = $_POST['nome'];
+$login = $_POST['login'];
+$setor = $_POST['setor'];
+$senha = $_POST['senha'];
 
 include 'conexao.php';
 
-$insert = "INSERT INTO tb_user VALUES (NULL,'nome','login','setor','senha',)";
+$insert = "INSERT INTO tb_user VALUES (NULL,'$nome','$login','$setor','$senha')";
 
-$query = mysqli_query($conexao, $insert) or die(mysqli_error($conn));
+$query = mysqli_query($conexao, $insert);
 ?>
